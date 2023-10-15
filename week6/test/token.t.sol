9de091aff168a006a3e123e9c67c2156e76f79e6
LouisTsai <q1030176@gmail.com>
2023-10-15 10:39:48 +0800

    test: deposit eth amount equal erc20 mint amount

--- week6/test/token.t.sol
+++ week6/test/token.t.sol
@@ -12,4 +12,21 @@ contract WrappedTokenTest is Test {
         weth = new WETH();
     }
 
+    function test_DepositTokenAmountEqualMintAmount() public {
+        // create user account
+        address user = makeAddr("user");
+        // set user address for the following function call
+        vm.startPrank(user);
+        // set ether balance for user
+        deal(user, 1 ether);
+        // user deposit 1 ether balance to the contract
+        weth.deposit{value: 1 ether}();
+        // check ERC20 balance for user
+        uint256 receiveERC20Amount = weth.balanceOf(user);
+        // compare user ERC20 token amount and sent ether amount
+        assertEq(receiveERC20Amount, 1 ether);
+        // stop prank and exit the test
+        vm.stopPrank();
+    }
+
 }
